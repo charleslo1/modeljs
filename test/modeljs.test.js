338703de3a177bb6a09bee5049a9a8c86a2d0fc1
Charles Lo <charleslo@sina.com>
2018-12-21 09:53:42 +0800

    增加 model.clone 测试代码

--- test/modeljs.test.js
+++ test/modeljs.test.js
@@ -6,22 +6,32 @@ global.wx = {
 const assert = require('assert')
 
 const Model = require('../dist/modeljs')
+const User = Model.define('User', {
+  id: {
+    type: Number,
+    default: 0
+  },
+  name: {
+    type: String,
+    default: 'name'
+  }
+})
 
 // 测试 modeljs.js
 describe('modeljs.js', () => {
 
   it('Model.define(props)', () => {
-    const User = Model.define('User', {
-      id: {
-        type: Number,
-        default: 0
-      },
-      name: {
-        type: String,
-        default: 'name'
-      }
-    })
     var user = new User({ id: 100, name: 'Charles Lo'})
     assert.equal('Charles Lo', user.name)
+  })
+
+  it('model.clone()', () => {
+    var user = new User({ id: 100, name: 'Charles Lo'})
+    var newUser1 = user.clone()
+    var newUser2 = User.clone(user)
+    assert.notEqual(newUser1, user)
+    assert.notEqual(newUser2, user)
+    assert.notEqual(newUser1, newUser2)
+    assert.equal(newUser1.name, newUser1.name)
   })
 })
